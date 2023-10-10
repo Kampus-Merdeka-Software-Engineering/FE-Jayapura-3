d7ca1a229ba6db020435552ebe32358787b1f880
narendra015 <narendrapamungkas07@gmail.com>
2023-10-10 07:21:09 +0700

    update

--- js/userlogin.js
+++ js/userlogin.js
@@ -1,28 +1,7 @@
 import { useFetch } from "/js/lib/fetch.js";
 
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const userlogin = document.querySelector(".userlogin");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 const formSignup = document.querySelector("#form-signup");
 const formSignin = document.querySelector("#form-signin");
-
-sign_up_btn.addEventListener("click", () => {
-  userlogin.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  userlogin.classList.remove("sign-up-mode");
-});
-
-sign_up_btn2.addEventListener("click", () => {
-  userlogin.classList.add("sign-up-mode2");
-});
-
-sign_in_btn2.addEventListener("click", () => {
-  userlogin.classList.remove("sign-up-mode2");
-});
 
 formSignup.addEventListener("submit", async (e) => {
   e.preventDefault();
